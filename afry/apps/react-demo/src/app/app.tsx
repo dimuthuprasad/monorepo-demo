80afc8826cdb9670f69ff1c9f681d72bfb60cac9
Dimuthu <idimuthu@gmail.com>
2022-02-09 13:36:48 +0100

    * initial changes

---
+++ afry/apps/react-demo/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+
+export function App() {
+  return (
+    <>
+  
+
+    </>
+  );
+}
+
+export default App;
