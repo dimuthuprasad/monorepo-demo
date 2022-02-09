6835b6b2688b427047c3b23406dcee986a6c9f61
Dimuthu <idimuthu@gmail.com>
2022-02-09 16:47:21 +0100

    * second react app

---
+++ afry/apps/react-demo-2/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import {Header} from '@afry/common-ui';
+
+export function App() {
+  return (
+    <>
+      <Header title="I am reusable UI component in demo app 2" />
+      <div />
+    </>
+  );
+}
+
+export default App;
